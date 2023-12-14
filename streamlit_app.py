7e7d9d0703cb8c8c9020a34623db6b5b7672c47f
thomasanthoinemilhomme <150704776+thomasanthoinemilhomme@users.noreply.github.com>
2023-12-14 18:03:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+st.title('Welcome to Our App')
+st.write('This is the home page of our application. You can choose to test the model or see our work.')
+
+if st.button('Test Model'):
+    # Code to handle the 'Test Model' action
+    st.write('You clicked to test the model!')
+
+if st.button('See Our Work'):
+    # Code to handle the 'See Our Work' action
+    st.write('You clicked to see our work!')
