cd0561467602dd712d36d3d71a6f9060bbb71d17
thomasanthoinemilhomme <150704776+thomasanthoinemilhomme@users.noreply.github.com>
2023-12-19 13:30:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,51 @@
-python -m venv myenv
-source myenv/bin/activate  # On Windows use `myenv\Scripts\activate`
-pip install streamlit transformers
+
 import streamlit as st
-from transformers import CamembertTokenizer, CamembertForSequenceClassification
+import pandas as pd
 import torch
+from transformers import CamembertTokenizer, CamembertForSequenceClassification
+import gdown
 
-# Load the tokenizer and model
+# Function to download and load the model
+def load_model():
+    url = 'https://drive.google.com/drive/folders/1wmxeEpWoQnzCBBRyzstq2BxmSRNE1AHf'
+    output = 'my_model.zip'
+    gdown.download(url, output, quiet=False)
+
+    # Extract the model
+    import zipfile
+    with zipfile.ZipFile(output, 'r') as zip_ref:
+        zip_ref.extractall('my_model')
+
+    # Load the model
+    model = CamembertForSequenceClassification.from_pretrained('my_model')
+    return model
+
+# Streamlit page configuration
+st.set_page_config(page_title="Language Level Detector", page_icon="🇫🇷")
+
+# Title and project description
+st.title("Detecting Language Level in French Sentences")
+st.markdown("## Project conducted by Team Lausanne with Leo Andrad and Thomas Anthoine-Milhomme")
+
+# User input
+user_input = st.text_input("Enter a French sentence to analyze its language level:")
+
+# Load model (this should be done only once, you might want to optimize this part)
+model = load_model()
+
+# Tokenizer
 tokenizer = CamembertTokenizer.from_pretrained('camembert-base')
-model = CamembertForSequenceClassification.from_pretrained('https://huggingface.co/tommilhomme/languagelevel/tree/main/my_model')
 
-st.title('French Sentence Difficulty Predictor')
+# Predict function
+def predict(sentence):
+    inputs = tokenizer(sentence, return_tensors="pt")
+    outputs = model(**inputs)
+    predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+    return predictions
 
-# User input for the sentence
-sentence = st.text_input('Enter a French sentence:')
-
-if sentence:
-    # Tokenize the input sentence
-    inputs = tokenizer(sentence, return_tensors='pt', truncation=True, padding=True)
-
-    # Predict the difficulty level
-    with torch.no_grad():
-        logits = model(**inputs).logits
-
-    # Convert logits to probabilities and then to the corresponding class
-    probs = torch.nn.functional.softmax(logits, dim=-1)
-    prediction = torch.argmax(probs, dim=-1).item()
-
-    # Define a mapping for difficulty levels
+# Display result
+if user_input:
+    prediction = predict(user_input)
     difficulty_mapping = {0: 'A1', 1: 'A2', 2: 'B1', 3: 'B2', 4: 'C1', 5: 'C2'}
-
-    # Display the predicted difficulty level
-    st.write(f'Predicted difficulty level: {difficulty_mapping[prediction]}')
+    predicted_level = difficulty_mapping[torch.argmax(prediction).item()]
+    st.write(f"The predicted language level for this sentence is: **{predicted_level}**")
